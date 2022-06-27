caa03440a7cb29cf9db60ecded0841c1d78cd8e6
Igor <filinigor@yandex.ru>
2022-06-27 17:25:36 +0300

    add new components 1-20 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import Dialogs from './components/Dialogs/Dialogs';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-
-
+import {Route} from "react-router-dom";
 
 
 function App() {
@@ -14,8 +13,8 @@ function App() {
             <Header/>
             <Navbar/>
             <div className="app-wrapper-content">
-                <Dialogs messages={'Hello'}/>
-                {/*<Profile/>*/}
+                <Route path={'/profile'} render={() => <Profile/>}/>
+                <Route path={'/dialogs'} render={() => <Dialogs messages={'HI'}/>}/>
             </div>
         </div>
     );
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,16 @@
 import classes from './Navbar.module.css'
+import {Link, NavLink} from "react-router-dom";
 
 
 function Navbar() {
     return (<div className={classes.navbar}>
         <div className={classes.items}>
-            <div className={classes.item}><a href='/profile'>Profile</a></div>
-            <div className={classes.item}><a href='/dialogs'>Message</a></div>
-            <div className={classes.item}><a href='#'>News</a></div>
-            <div className={classes.item}><a href='#'>Music</a></div>
-            <div className={classes.item}><a href='#'>Settings</a></div>
+            <div className = {classes.item}><NavLink to="/profile">Profile</NavLink></div>
+            <div className={classes.item}><Link to="/dialogs">Message</Link></div>
+            <div className={classes.item}><Link to="/news">News</Link></div>
+            <div className={classes.item}><Link to="/music">Music</Link></div>
+            <div className={classes.item}><Link to="/settings">Settings</Link></div>
         </div>
-
     </div>)
 }
 
