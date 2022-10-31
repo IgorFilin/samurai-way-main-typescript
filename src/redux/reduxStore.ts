dce0bc31cf4f1f26c673eb037e7a68bc2eebdf70
Igor <filinigor@yandex.ru>
2022-10-31 18:25:37 +0300

    connect in app ReduxDevTools

--- src/redux/reduxStore.ts
+++ src/redux/reduxStore.ts
@@ -5,32 +5,44 @@ import {SidebarReducer} from "./SidebarReducer";
 import {AllActionCreatorsTypeUser, UsersReducer} from "./UsersReducer";
 import {AllActionsCreatorsTypeAuth, AuthReducer} from "./authReducer";
 import MiddleWareThunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
-import { reducer as formReducer } from 'redux-form'
+import {reducer as formReducer} from 'redux-form'
 import {AllActionsCreatorsTypeApp, appReducer} from "./appReducer";
+import {composeWithDevTools} from "@redux-devtools/extension";
 
 
 let rootReducer = combineReducers({
     profilePage: ProfileReducer,
     dialogPage: DialogsReducer,
     sidebar: SidebarReducer,
-    userPage:UsersReducer,
-    auth:AuthReducer,
-    form:formReducer,
-    app:appReducer
+    userPage: UsersReducer,
+    auth: AuthReducer,
+    form: formReducer,
+    app: appReducer
 })
 
-export let store = createStore(rootReducer,applyMiddleware(MiddleWareThunk))
+export const store = createStore(
+    rootReducer,
+    composeWithDevTools(
+        applyMiddleware(MiddleWareThunk)
+        // other store enhancers if any
+    )
+);
 
 // @ts-ignore
 window.store = store
 
-export type AppDispatch = ThunkDispatch<StateType,unknown,DomainActionsCreatorsType>
+export type AppDispatch = ThunkDispatch<StateType, unknown, DomainActionsCreatorsType>
 
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType,StateType,unknown,DomainActionsCreatorsType>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, StateType, unknown, DomainActionsCreatorsType>
 
-export type DomainActionsCreatorsType = AllActionsCreatorsTypeApp | AllActionsCreatorsTypeAuth | AllActionsCreatorsProfile | AllActionsCreatorsTypeDialogs | AllActionCreatorsTypeUser
+export type DomainActionsCreatorsType =
+    AllActionsCreatorsTypeApp
+    | AllActionsCreatorsTypeAuth
+    | AllActionsCreatorsProfile
+    | AllActionsCreatorsTypeDialogs
+    | AllActionCreatorsTypeUser
 
-export type StateType =  ReturnType<typeof rootReducer>
+export type StateType = ReturnType<typeof rootReducer>
 
 export type DispatchType = Dispatch
 
